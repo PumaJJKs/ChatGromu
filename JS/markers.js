b9ddbbe1099298bdd147153abbb0874d121e3132
PumaJJKs <feliperuivomartins@gmail.com>
2026-02-12 09:06:15 +0000

    Fix API and paths

--- JS/markers.js
+++ JS/markers.js
@@ -1,4 +1,4 @@
-import { marked } from 'https://cdn.jsdelivr.net/npm/marked/marked.min.js';
+import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js';
 
 // Configuração do marked
 marked.setOptions({
